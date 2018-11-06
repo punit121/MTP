c28daa9ba0e6990d8d0eb544b57c3896ab1538ec
Punit kumar <punit.kumar1661@gmail.com>
2018-11-07 01:00:55 +0530

    Add files via upload

---
+++ classify.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import sys
+import os
+import json
+import numpy as np
+
+#import tkinter as tk
+#from tkinter import filedialog
+
+# Disable tensorflow compilation warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+image_path = sys.argv[1]
+
+#root = tk.Tk()
+#root.withdraw()
+
+#mage_path = filedialog.askopenfilename()
+class MyEncoder(json.JSONEncoder):
+    def default(self,obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+        return json.dumps(data, cls=MyEncoder)
+
+
+if image_path:
+    
+    # Read the image_data
+    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+    # Loads label file, strips off carriage return
+    label_lines = [line.rstrip() for line 
+                       in tf.gfile.GFile("tf_files/retrained_labels.txt")]
+
+    # Unpersists graph from file
+    with tf.gfile.FastGFile("tf_files/retrained_graph.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+    with tf.Session() as sess:
+        # Feed the image_data as input to the graph and get first prediction
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        
+        predictions = sess.run(softmax_tensor, \
+                 {'DecodeJpeg/contents:0': image_data})
+        
+        # Sort to show labels of first prediction in order of confidence
+        my_dict = {}
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+        
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+          #print('%s (score = %.5f)' % (human_string, score))
+            human_string = human_string.replace(',', '')
+            #human_string = human_string.replace(' ', '_')
+            my_dict[human_string] = score
+            #print('%s (score = %.5f)' % (human_string, score))
+
+        result= json.dumps(my_dict, cls=MyEncoder)
+        #json_data = json.loads(result)
+        print(json.loads(result))    
